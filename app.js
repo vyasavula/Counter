4668c46b674f833e41ef33e271f2579373644ea5
your username <vyasavula6@gmail.com>
2021-08-22 04:35:12 +0530

    Intial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+//set Intial count
+
+let count = 0;
+
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach(function(btn) {
+    btn.addEventListener("click", function(e) {
+        console.log(e);
+        const styles = e.currentTarget.classList;
+        console.log(styles);
+        if (styles.contains("decrease")) {
+            count--;
+        } else if (styles.contains("increase")) {
+            count++;
+        } else {
+            count = 0;
+        }
+        value.textContent = count;
+    });
+});
